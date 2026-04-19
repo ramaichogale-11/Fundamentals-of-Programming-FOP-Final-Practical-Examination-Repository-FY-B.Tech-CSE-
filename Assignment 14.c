e4a811198d5097587182879a85e9e771d1526a2d
ramaichogale-11 <ramaichogale@gmail.com>
2026-04-19 21:10:43 +0530

    Assignment 14.c

--- Assignment 14.c
+++ Assignment 14.c
@@ -1,20 +1,20 @@
-// Binary to Decimal
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-    int binary,n=0,i=0,r;
+int main() {
+    int binary, decimal = 0, weight = 1, rem;
 
-    scanf("%d",&binary);
+    printf("Enter a binary number: ");
+    scanf("%d", &binary);
 
-    while(binary!=0){
-        r=binary%10;
-        n+=r*pow(2,i);
-        binary/=10;
-        i++;
+    while (binary != 0) {
+        rem = binary % 10;
+        decimal = decimal + (rem * weight);
+        binary = binary / 10;
+        weight = weight * 2;
     }
 
-    printf("%d",n);
+    printf("Decimal equivalent: %d\n", decimal);
 
     return 0;
 }
